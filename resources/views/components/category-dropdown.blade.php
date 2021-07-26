7c45155c0475f6f3c5eca8d648625f25e23efd03
AzadGh95 <azad.gh.1995@gmail.com>
2021-07-26 11:39:08 +0330

    commit-42

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -3,7 +3,7 @@
         <button
             class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex">
             {{isset($currentCategory)?ucwords($currentCategory->name):'Category'}}
-            <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;" />
+            <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;"/>
         </button>
     </x-slot>
     <x-dropdown-item
@@ -15,9 +15,10 @@
 
     @foreach($categories as $category)
         <x-dropdown-item
-            href="/?category={{ $category->slug }}"
+            href="/?category={{ $category->slug }}&{{http_build_query(request()->except('category'))}}"
             :active='request()->is("categories/{$category->slug}")'
-        >{{ucwords($category->name)}}</x-dropdown-item>
+        >{{ucwords($category->name)}}
+        </x-dropdown-item>
     @endforeach
 
 </x-dropdown>
