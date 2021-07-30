3933aa09130663ceae47bd855f02bdd356a8bcfa
AzadGh95 <azad.gh.1995@gmail.com>
2021-07-30 05:42:02 +0430

    commit-45
    Build a register user pages

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,11 +16,7 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for arrays.
@@ -41,7 +37,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function posts(){
+    public function posts()
+    {
         return $this->hasMany(Post::class);
     }
 }
