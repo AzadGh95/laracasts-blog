942b26477f8be39130a4debd9aebd422c609738d
AzadGh95 <azad.gh.1995@gmail.com>
2021-07-30 15:09:32 +0430

    commit-48
    flash message

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -18,12 +18,11 @@ class RegisterController extends Controller
         $attributes = request()->validate([
             'name' => 'required|max:255',
             'username' => 'required|min:3|max:255|unique:users,username',
-//            'username' => ['required','min:3','max:255',Rule::unique('users','username')->ignore()],
             'email' => 'required|email|min:3|max:255|unique:users,email',
             'password' => 'required|min:7|max:255',
         ]);
-//        $attributes['password'] = bcrypt($attributes['password']);
         User::create($attributes);
-        return redirect('/');
+//        session()->flash('success','Your account has been created.');
+        return redirect('/')->with('success', 'Your account has been created.');
     }
 }
