7759cd6e1a17f5a1741c6cca315dba3b913a2a4e
AzadGh95 <azad.gh.1995@gmail.com>
2021-08-01 14:26:48 +0430

    commit-56

--- app/Models/Commit.php
+++ app/Models/Commit.php
@@ -16,7 +16,7 @@ class Commit extends Model
 
     public function author()
     {
-        return $this->belongsTo(User::class , 'user_id');
+        return $this->belongsTo(User::class, 'user_id');
     }
 
 }
--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -2,13 +2,13 @@
 <x-panel class="bg-gray-50">
 <article class="flex space-x-4 my-6">
     <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/70?u={{$commit->id}}" alt="" width="70" height="70" class="rounded-xl">
+        <img src="https://i.pravatar.cc/70?u={{$commit->user_id}}" alt="" width="70" height="70" class="rounded-xl">
     </div>
     <div>
         <header class="mb-4">
             <h3 class="font-bold">{{ $commit->author->username }}</h3>
             <p class="text-xs">Posted
-                <time>{{$commit->created_at}}</time>
+                <time>{{$commit->created_at->format('F j, Y, g:i a')}}</time>
             </p>
         </header>
         <p>{{$commit->body}}</p>
