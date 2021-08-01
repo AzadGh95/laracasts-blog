6861dd03f3c294ee942e90fec224ce5c394c4c14
AzadGh95 <azad.gh.1995@gmail.com>
2021-08-01 22:15:48 +0430

    commit-58
    https://docs.bolt.cm/4.0/howto/curl-ca-certificates

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,22 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
+use MailchimpMarketing\ApiClient;
+
+Route::get('ping', function () {
+    $mailchimp = new ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us6'
+    ]);
+
+    $response = $mailchimp->lists->addListMember('0d590e843d', [
+        'email_address' => 'azad.gh.1991@gmail.com',
+        'status' => 'subscribed'
+    ]);
+    ddd($response);
+});
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
